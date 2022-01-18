a5311a46fb5e2c6f10f73fe9c8bcb07af3ba0e40
LeviSilvaz99 <contatolevisilva@outlook.com>
2022-01-17 23:06:29 -0300

    initial commit

---
+++ src/main/java/com/testeubots/arvore/Elemento.java
@@ -0,0 +1,17 @@
+package com.testeubots.arvore;
+
+public class Elemento {
+    private int valor;
+
+    public Elemento(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+}
